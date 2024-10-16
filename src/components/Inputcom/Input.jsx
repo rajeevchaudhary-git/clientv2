fd837b37f698c7baf05aed3ac0171340abff4bae
Rajeev chaudhary <91446278+rajeevchaudhary-git@users.noreply.github.com>
2024-10-16 16:42:53 +0530

    Update Input.jsx sakshi jain

--- src/components/Inputcom/Input.jsx
+++ src/components/Inputcom/Input.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 function Input({
   label = '',
   name = '',
-  type='',
-  className='',
-  placeholder='enter the text',
+  type = '',
+  className = '',
+  placeholder = 'Enter the text',
   isRequired = false,
-  onchange =()=>{},
-  value=""
-   
+  onchange = () => {},
+  value = ''
 }) {
- return (
-    <div className='w-1/2'>
+  return (
+    <div className="w-1/2">
       <label htmlFor={name} className="block mb-2 text-sm font-medium text-slate-200">
         {label}
       </label>
       <input
         type={type}
         id={name}
-        className={bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
         placeholder={placeholder}
-        required={isRequired} onChange={onchange} value={value}
+        required={isRequired}
+        onChange={onchange}
+        value={value}
       />
     </div>
-  )
+  );
 }
 
-export default Input
-
+export default Input;
