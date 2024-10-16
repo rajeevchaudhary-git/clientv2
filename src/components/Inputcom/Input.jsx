a55a0f5c29006134285ad362a5a14813e978e8c9
Rajeev chaudhary <91446278+rajeevchaudhary-git@users.noreply.github.com>
2024-10-16 16:40:40 +0530

    Update Input.jsx sakshi jain

--- src/components/Inputcom/Input.jsx
+++ src/components/Inputcom/Input.jsx
@@ -11,15 +11,15 @@ function Input({
   value=""
    
 }) {
-  return (
+ return (
     <div className='w-1/2'>
-      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-800">
+      <label htmlFor={name} className="block mb-2 text-sm font-medium text-slate-200">
         {label}
       </label>
       <input
         type={type}
         id={name}
-        className={`bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
+        className={bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}}
         placeholder={placeholder}
         required={isRequired} onChange={onchange} value={value}
       />
