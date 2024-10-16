3268062c2be4f39bc2fe589dc6911d680ad77143
Rajeev chaudhary <91446278+rajeevchaudhary-git@users.noreply.github.com>
2024-10-16 16:44:20 +0530

    Update Button.jsx sakshi jain

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -6,11 +6,13 @@ function Button({
     label = 'Enter label',
     disabled = false,
 }) {
-   return (
-        <div className='w-1/2'>
+    return (
+        <div className="w-1/2">
             <button 
                 type={type}
-                className={text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm   px-5 py-2.5 text-center  ${className}} disabled={disabled}>
+                className={`text-white bg-purple-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${className}`}
+                disabled={disabled}
+            >
                 {label}
             </button>
         </div>
